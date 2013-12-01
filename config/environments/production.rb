0ed04098d6ea231c7b82cb76a895c85189fe8dd1
RichardWieditz <richardwieditz@gmail.com>
2013-12-01 01:03:24 +0100

    heoku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,6 @@ LandgasthausKoehler::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
@@ -77,4 +76,5 @@ LandgasthausKoehler::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.threadsafe!
 end
