5dcb0a839986dbd00d209c87df4a5216bf6bf2a1
RichardWieditz <richardwieditz@gmail.com>
2013-12-01 01:14:23 +0100

    heroku

--- app/controllers/frontend/catering/dynamic_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class Frontend::Catering::DynamicPagesController < Frontend::Catering::ApplicationController
-  Page.catering.each do |page|
-    define_method page.title.downcase.to_sym do
-      render :file => "layouts/dynamic_app", :layout => false
-    end
-  end
-end
