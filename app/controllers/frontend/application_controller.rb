7859650924b7e1bd2dc8ae71cf41ec5372e1d6fb
RichardWieditz <richardwieditz@gmail.com>
2014-01-03 22:59:29 +0100

    heroku bugg fixing

--- app/controllers/frontend/application_controller.rb
+++ app/controllers/frontend/application_controller.rb
@@ -5,6 +5,6 @@ class Frontend::ApplicationController < ApplicationController
   protected
     def globalize_page
       @page = Contentr::Page.find_by(slug: request.path[1..request.path.length].parameterize)
-      @paragraphs = @page.paragraphs.select{|p| (p.area_name == 'body' && p.body.present?)}
+      @paragraphs = @page.paragraphs.select{|p| (p.area_name == 'body' && p.body.present?)} if @page.present?
     end
 end
