b01611a9291acbc0be2866d133a3e4be35b45700
RichardWieditz <richardwieditz@gmail.com>
2014-02-03 14:41:52 +0100

    latest update

--- app/models/contentr/scroller_image_paragraph.rb
+++ app/models/contentr/scroller_image_paragraph.rb
@@ -5,6 +5,7 @@ module Contentr
       field :desktop_image, :uploader => Contentr::FileUploader
       field :tablet_image, :uploader => Contentr::FileUploader
       field :phone_image, :uploader => Contentr::FileUploader
+      field :body, type: "String"
       # Validations
     end
   end
