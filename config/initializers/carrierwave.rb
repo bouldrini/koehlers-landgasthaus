8862a750b9b8a55fffd011e70ec5590a9289cb0f
RichardWieditz <richardwieditz@gmail.com>
2014-03-05 10:59:03 +0100

    sec

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,9 @@ ENV["AWS_S3_BUCKET"] = 'koehlers-landgasthaus'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => nil,
-    :aws_secret_access_key  => nil
+    :aws_access_key_id      => 'AKIAI2MNIUEW5CVOKPKA',
+
+    :aws_secret_access_key  => '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'
   }
   config.fog_directory  = ENV["AWS_S3_BUCKET"]
 end
